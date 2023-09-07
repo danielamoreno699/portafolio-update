5229eb586a739411b43c6267c68c316d0ec3c59a
Daniela Moreno <dani9292@hotmail.com>
2023-09-07 13:27:23 -0500

    update css for profile

--- src/helper/topproj.js
+++ src/helper/topproj.js
@@ -2,7 +2,21 @@ import wanderpal from '../asset/images/projects_imgs/wanderpal-resize.png';
 
 const projectsTop = [
   {
-    id: 0,
+    id: 1,
+    name: 'Wanderpal Fullstack',
+    description: `The app is built using React with Vite, which are technologies used for creating web applications. React is a popular JavaScript library for building user interfaces, and Vite is a build tool that enhances the development experience.
+
+The primary functionality of the app is to allow users to book reservations for tours. To access this functionality, users need to be logged in, suggesting that there's likely a user authentication system in place.
+
+The app supports different types of requests that users can execute. These requests include creating, retrieving (getting), and deleting reservations and items. This indicates that the app has a comprehensive set of features related to managing reservations and items associated with tours.`,
+    image: wanderpal,
+    year: '2023',
+    technologies: ['react', 'redux', 'bootstrap', 'rails', 'ruby', 'psql'],
+    repoLink: 'https://github.com/danielamoreno699/bookstore',
+    demoLink: '',
+  },
+  {
+    id: 2,
     name: 'Wanderpal Fullstack',
     description: `The app is built using React with Vite, which are technologies used for creating web applications. React is a popular JavaScript library for building user interfaces, and Vite is a build tool that enhances the development experience.
 
