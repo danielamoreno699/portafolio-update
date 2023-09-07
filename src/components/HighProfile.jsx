1fb4557044e63140c3d04af4da6b4cbfe66015f7
Daniela Moreno <dani9292@hotmail.com>
2023-09-07 10:02:03 -0500

    update css for profile

--- src/components/HighProfile.jsx
+++ src/components/HighProfile.jsx
@@ -2,39 +2,36 @@
 import Card from 'react-bootstrap/Card';
 import '../styles/profile.css'; 
 import { BsGithub } from 'react-icons/bs';
+import { useSelector } from 'react-redux';
+
 
 
 const HighProfile = () => {
+  const { top } = useSelector((state) => state.top);
 
-  
   return (
     <section id="high-profile">
-        
       <div className='high-profile'>
-     
-        <div className='high-cont ' >
-        <div className='mask'></div>
-        <h2 className='h2-top'>my top projects</h2>
-        
-        
-          <Card className='card-cont' >
-            <Card.Img className='card-img' variant="left" src="path/to/your/image.jpg" /> 
-            <Card.Body className='card-body-profile'>
-              <Card.Title className='profile-title'>Card Title</Card.Title>
-              <Card.Text className='profile-text'>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, consequuntur! Nihil cum facilis odit nam omnis doloremque laborum minima impedit.
-               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi minus porro beatae quaerat omnis qui totam fugit debitis nobis quasi.
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam voluptate nam reiciendis dicta. Perferendis odit praesentium voluptas culpa corrupti reprehenderit?
-              </Card.Text>
-              <button type="button" className="btn-see-more git" >
-              See GitRepo
-              <BsGithub className="git-icon" />
-            </button>
-            </Card.Body>
-        
-          </Card>
+        <div className='high-cont '>
+          <div className='mask'></div>
+          <h2 className='h2-top'>my top projects</h2>
 
-    
+       
+          {top.map((project, index) => (
+            <Card className='card-cont' key={index}>
+              <Card.Img className='card-img' variant="left" src={project.imageUrl} alt='img' />
+              <Card.Body className='card-body-profile'>
+                <Card.Title className='profile-title'>{project.title}</Card.Title>
+                <Card.Text className='profile-text'>
+                  {project.description}
+                </Card.Text>
+                <button type="button" className="btn-see-more git">
+                  See GitRepo
+                  <BsGithub className="git-icon" />
+                </button>
+              </Card.Body>
+            </Card>
+          ))}
         </div>
       </div>
     </section>
--- src/helper/topproj.js
+++ src/helper/topproj.js
@@ -1,3 +1,5 @@
+import wanderpal from '../asset/images/projects_imgs/wanderpal.png';
+
 const projectsTop = [
   {
     id: 0,
@@ -7,7 +9,7 @@ const projectsTop = [
 The primary functionality of the app is to allow users to book reservations for tours. To access this functionality, users need to be logged in, suggesting that there's likely a user authentication system in place.
 
 The app supports different types of requests that users can execute. These requests include creating, retrieving (getting), and deleting reservations and items. This indicates that the app has a comprehensive set of features related to managing reservations and items associated with tours.`,
-    imageLink: '',
+    imageLink: wanderpal,
     year: '2023',
     technologies: ['react', 'redux', 'bootstrap', 'rails', 'ruby', 'psql'],
     repoLink: 'https://github.com/danielamoreno699/bookstore',
