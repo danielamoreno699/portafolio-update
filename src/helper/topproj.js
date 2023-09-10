2d278d104d6079928b5eaa7695da9995738527de
Daniela Moreno <dani9292@hotmail.com>
2023-09-10 02:36:44 -0500

    update css

--- src/helper/topproj.js
+++ src/helper/topproj.js
@@ -27,11 +27,7 @@ const projectsTop = [
   {
     id: 2,
     name: 'Wanderpal Fullstack',
-    description: `The app is built using React with Vite, which are technologies used for creating web applications. React is a popular JavaScript library for building user interfaces, and Vite is a build tool that enhances the development experience.
-
-The primary functionality of the app is to allow users to book reservations for tours. To access this functionality, users need to be logged in, suggesting that there's likely a user authentication system in place.
-
-The app supports different types of requests that users can execute. These requests include creating, retrieving (getting), and deleting reservations and items. This indicates that the app has a comprehensive set of features related to managing reservations and items associated with tours.`,
+    description: `The app is built using React with Vite, which are technologies used for creating web applications and items.`,
     image: wanderpal,
     year: '2023',
     technologies: ['react', 'redux', 'bootstrap', 'rails', 'ruby', 'psql'],
