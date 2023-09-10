d6397b6bcf7af2d6554d1678f9787413b8c86f51
Daniela Moreno <dani9292@hotmail.com>
2023-09-09 23:39:48 -0500

    add routes

--- src/components/projectInfo.jsx
+++ src/components/projectInfo.jsx
@@ -3,6 +3,14 @@ import { CgLivePhoto } from 'react-icons/cg';
 import { useProjectData } from '../context/contextManager';
 import '../styles/projectInfo.css'
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+
 const ProjectInfo = () => {
 
   const { projectData } = useProjectData();
@@ -10,23 +18,46 @@ const ProjectInfo = () => {
 
   return (
     <div className='project-info-details'>
+   
+   <div className='scrollable-container'>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+          
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          spaceBetween={90}
+        // spaceBetween={0}
+        slidesPerView={2}
+        direction={'vertical'}
+        // direction="vertical"
+   
+        // pagination={{ clickable: true }}
+        // scrollbar={{ draggable: true }}
+      >
+        <SwiperSlide>
+          <div className='image-stack_item'>
+            <img className='project-img-1' src={projectData.imageProjectItem1} alt='img' />
+          </div>
+          
+        </SwiperSlide>
         
-      {/* <h3>{projectData.name}</h3>
-      */}
-      
-      <div className='scrollable-container'>
-    <div className='image-stack'>
-      <div className='image-stack_item'>
-        <img className='project-img-1' src={projectData.imageProjectItem1} alt='img' />
-      </div>
-      <div className='image-stack_item'>
-        <img className='project-img-2' src={projectData.imageProjectItem2} alt='img' />
-      </div>
-      <div className='image-stack_item'>
-        <img className='project-img-3' src={projectData.imageProjectItem3} alt='img' />
-      </div>
+        <SwiperSlide>
+        <div className='image-stack_item'>
+            <img className='project-img-2' src={projectData.imageProjectItem2} alt='img' />
+          </div>
+
+        </SwiperSlide>
+        
+        <SwiperSlide>
+          <div className='image-stack_item'>
+            <img className='project-img-3' src={projectData.imageProjectItem3} alt='img' />
+          </div>
+
+        </SwiperSlide>
+   
+      </Swiper>
     </div>
-  </div>
 
 
       <div className='desc-text-info-details static'>
