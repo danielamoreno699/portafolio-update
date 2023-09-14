37385453efcb1b8e3a40d412fa9c96880bb2aac6
Daniela Moreno <dani9292@hotmail.com>
2023-09-14 01:58:13 -0500

    update css

--- src/helper/aboutme.js
+++ src/helper/aboutme.js
@@ -2,8 +2,8 @@ const paragraphsAbout = [
     "As an experienced attorney and financial analyst, I have a strong passion for research and I'm eager to enhance my programming abilities. With a keen understanding of the increasing significance of technology in the legal and financial industries, I am currently enrolled in the Microverse Software Engineering Bootcamp.",
     "My career began as a lawyer, during which I earned a Master's degree from the renowned University of Glasgow in Scotland. This rigorous academic experience instilled in me the invaluable ability to conduct meticulous research, perform insightful analysis, deliver logical and articulate statements, and above all, refine my skills in persuasive communication.",
     "After working as a lawyer for 2 years in the corporate field, I became enthusiastic toward the financial markets. I worked as an External consultant for an important investor. Through that experience, I gained a deeper understanding of data interpretation and analysis, particularly through the application of fundamental and technical analysis techniques. It was during this time that I realized the critical role that programming plays in making informed strategic decisions, particularly in the realm of investing.",
-    "Now, I'm eager to leverage my legal and financial expertise with my programming skills to drive innovation and efficiency in the legal and financial sectors. I believe that my unique combination of skills will enable me to contribute to cutting-edge projects and create solutions that bridge the gap between technology and law/finance.",
-    "If you are seeking a versatile professional who can navigate complex legal and financial landscapes while harnessing the power of programming, I would welcome the opportunity to discuss how I can contribute to your organization's success. Let's connect and explore the possibilities."
+    "Now, I'm eager to leverage my legal and financial expertise with my programming skills to drive innovation and efficiency in the legal and financial sectors. ",
+
   ];
 
   export default paragraphsAbout
